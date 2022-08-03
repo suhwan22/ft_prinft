1b3e2cb44c6935bdeb18d4f7358e9046b8eae440
Suhwan Kim <suhkim@c4r5s1.42seoul.kr>
2022-08-03 18:44:24 +0900

    ft_printf third commit

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: suhkim <suhkim@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/03 16:58:23 by suhkim            #+#    #+#             */
-/*   Updated: 2022/08/03 18:36:17 by suhkim           ###   ########.fr       */
+/*   Updated: 2022/08/03 18:42:30 by suhkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,7 @@
 # include <unistd.h>
 # include <stdarg.h>
 
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
-
 int	ft_printf(const char *format, ...);
-
 int	ft_putnbr_base(unsigned long nbr, char *base, int len, int *bytes);
 
 #endif
